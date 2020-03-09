915ba02ff0d28ba81b0c72ff6429b8349fa3346e
Mat Cartmill <mat.cartmill@gmail.com>
2020-03-09 17:10:13 -0400

    Add basic configuration parsing test (#7)
    * Initial tests
    
    * Remove template test
    
    * Remove tmp files
    
    * Add line break
    
    * Remove test_helper
    
    * Import only required class
    
    * Add new line
    
    * use proper casing

---
+++ test/tests/config_test.rb
@@ -0,0 +1,11 @@
+require 'enigma/config'
+require 'minitest/autorun'
+
+class ConfigTest < MiniTest::Test
+  def test_parses_correctly
+    config = Enigma::Config.new(path: File.join(__dir__, '..', 'Enigmafile'))
+    assert_equal config.access_key_id, 'MY_AWS_KEY'
+    assert_equal config.secret_access_key, 'MY_AWS_SECRET'
+    assert_equal config.aws_region, 'us-east-2'
+  end
+end
