8475af900e5f30f25220f733b1f1fcb944e506d8
Mat Cartmill <mat.cartmill@gmail.com>
2020-03-10 09:13:07 -0400

    Use symbols instead of strings

--- test/tests/item_test.rb
+++ test/tests/item_test.rb
@@ -3,7 +3,7 @@ require 'minitest/autorun'
 
 class ItemTest < MiniTest::Test
   def test_parses_correctly_with_default_empty_value
-    hash = { 'key' => 'apiKey', 'property_name' => 'key' }
+    hash = { key: 'apiKey', property_name: 'key' }
     item = Enigma::Item.new(hash: hash)
 
     assert_equal item.key, 'apiKey'
