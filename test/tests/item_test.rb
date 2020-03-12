a8d42a60103e1a626783293300d7b64cbbfda21c
Mat Cartmill <mat.cartmill@gmail.com>
2020-03-12 09:53:17 -0400

    Add tests

--- test/tests/item_test.rb
+++ test/tests/item_test.rb
@@ -1,5 +1,7 @@
 require File.join(__dir__, '..', 'test_helper')
+require 'enigma/config'
 require 'enigma/item'
+require 'enigma/secret'
 require 'minitest/autorun'
 
 class ItemTest < MiniTest::Test
@@ -10,5 +12,17 @@ class ItemTest < MiniTest::Test
     assert_equal item.key, 'apiKey'
     assert_equal item.property_name, 'key'
     assert_equal item.value, ''
+  end
+
+  def test_parses_correctly_from_config
+    config = Enigma::Config.new(path: File.join(__dir__, '..', 'Enigmafile'))
+    secret = Enigma::Secret.new(hash: config.secrets[0])
+    item1 = Enigma::Item.new(hash: secret.items[0])
+    item2 = Enigma::Item.new(hash: secret.items[1])
+    
+    assert_equal item1.key, 'apiKey'
+    assert_equal item1.property_name, 'key'
+    assert_equal item2.key, 'apiSecret'
+    assert_equal item2.property_name, 'secret'
   end
 end
--- test/tests/secret_test.rb
+++ test/tests/secret_test.rb
@@ -1,13 +1,22 @@
 require File.join(__dir__, '..', 'test_helper')
+require 'enigma/config'
 require 'enigma/secret'
 require 'minitest/autorun'
 
 class SecretTest < MiniTest::Test
-  def test_parses_correctly
+  def test_parses_correctly_from_hash
     hash = { name: 'Secret', items: [{ key: 'apiKey', property_name: 'key' }, { key: 'apiSecret', property_name: 'secret' }]}
     secret = Enigma::Secret.new(hash: hash)
 
     assert_equal secret.name, 'Secret'
+    assert_equal secret.items.length, 2
+  end
+
+  def test_parses_correctly_from_config
+    config = Enigma::Config.new(path: File.join(__dir__, '..', 'Enigmafile'))
+    secret = Enigma::Secret.new(hash: config.secrets[0])
+    
+    assert_equal secret.name, 'someRandomThirdParty'
     assert_equal secret.items.length, 2
   end
 end
